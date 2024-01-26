92ebf69b1ed4e3c5cc94c30fb1a15247f22f8d23
alidd <moazenib.ali@gmail.com>
2024-01-26 16:04:38 +0330

    Add first command ( gb config user.name "name" )

--- gb.c
+++ gb.c
@@ -6,6 +6,18 @@
 
 int main(int argc , char *argv[])
 {
-
+    if      (!strcmp(argv[1] , "config") &&  !strcmp(argv[2] , "user.name" )) //git config user.name " "
+    {
+        FILE *file;
+        file = fopen(".gb/local-name.txt" , "w"); 
+        if(file == NULL)
+        {
+            printf("fatal: not in a gb directory\n");
+            return 0;
+        }
+        fprintf(file , "%s" , argv[argc - 1]);
+        fclose(file);
+    }
+    
     return 0;
 }
