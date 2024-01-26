a819be640616e5fe20cf4164481f50fb68773a44
alidd <moazenib.ali@gmail.com>
2024-01-26 16:21:19 +0330

    Add command ( git config alias.sth "sth" )

--- gb.c
+++ gb.c
@@ -83,5 +83,45 @@ int main(int argc , char *argv[])
         fclose(file);
     }
     
+    else if (!strcmp(argv[1] , "config") && !strncmp(argv[2] , "alias." , 6)) //git config alias.sth "sth"
+    {
+        FILE *file;
+        file = fopen("D:\\myproject\\files\\valid-commands.txt" , "r");
+
+        int isinvalid = 0;
+        char line[100];
+        fgets(line , 100 , file);
+        char *tokenPtr = strtok(line , "?");
+        while (tokenPtr != NULL) 
+        {
+            if (!strncmp(tokenPtr , argv[3] , strlen(tokenPtr)))
+            {
+                isinvalid = 1;
+                break;
+            }
+            tokenPtr = strtok(NULL, "?");
+        }
+
+        if (isinvalid == 1)
+        {
+            FILE *file1;
+            file1 = fopen(".gb\\alias-commands.txt" , "a");
+            if(file1 == NULL)
+            {
+                printf("fatal: not in a gb directory\n");
+                return 0;
+            }
+
+            fprintf(file1 , ">%s>%s?" , argv[2]+6 , argv[3]);
+            fclose(file1);
+        }
+
+        else
+            printf("The command you are trying to add an alias to is invalid\n");
+        
+        fclose(file);
+        
+    }
+    
     return 0;
 }
